dd8b5da613624c947d0018ff19f2c20365306419
Guillermo Iguaran <guilleiguaran@gmail.com>
2014-08-29 19:45:55 -0500

    Enable has_secure_password on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, confirmation: true
   validates :password_confirmation, presence: true
   validates :name, presence: true
-
-  attr_accessor :password, :password_confirmation
 end
